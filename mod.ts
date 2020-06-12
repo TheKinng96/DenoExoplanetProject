30aa312756777f4a46375b41c3d31243c5a9a688
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-12 15:05:41 +0900

    added condition on planet

--- mod.ts
+++ mod.ts
@@ -2,8 +2,12 @@ import { join } from "http://deno.land/std/path/mod.ts";
 import { BufReader } from "http://deno.land/std/io/bufio.ts";
 import { parse } from "http://deno.land/std/encoding/csv.ts";
 
+interface Planet {
+  [key: string]: string;
+}
+
 async function loadPlanetData() {
-  const path = join(".", "data.csv");
+  const path = join(".", "original.csv");
 
   const data = await Deno.open(path);
   const bufReader = new BufReader(data);
@@ -13,7 +17,19 @@ async function loadPlanetData() {
   });
   Deno.close(data.rid);
 
-  console.log(result);
+  const planets = (result as Array<Planet>).filter((planet) => {
+    const planetaryRadius = Number(planet["koi_prad"]);
+    const stellarMass = Number(planet["koi_smass"]);
+    const stellarRadius = Number(planet["koi_srad"]);
+
+    return planet["koi_disposition"] === "CONFIRMED" &&
+      planetaryRadius > 0.5 && planetaryRadius < 1.5 &&
+      stellarMass > 0.78 && stellarMass < 1.04 &&
+      stellarRadius > 0.99 && stellarRadius < 1.01;
+  });
+
+  return planets;
 }
 
-await loadPlanetData();
+const newEarths = await loadPlanetData();
+console.log(`${newEarths.length} habitable planets found!`);
